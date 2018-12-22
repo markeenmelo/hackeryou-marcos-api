a06aad21b725a18e0c2014f66c2565f8ab2f33fe
Marcos Melo <marcos.melo@bell.ca>
2018-12-22 01:12:42 -0500

    removed dotenv

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 const server = require('./server')
-require('dotenv').config()
 
 const PORT = process.env.PORT || 8080
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/hackerYouProject'
