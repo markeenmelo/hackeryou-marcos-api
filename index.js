8189c08778d1ae5db739b1dd6852caad3b72a88c
Marcos Melo <marcos.melo@bell.ca>
2018-12-22 01:11:43 -0500

    add mongodb

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
 const server = require('./server')
+require('dotenv').config()
 
-var port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/hackerYouProject'
 
-
-server.listen(port, async () => {
-    await mongoose.connect(process.env.DB_URI, {useNewUrlParser: true})
-    console.log(`Database connected at ${process.env.DB_URI}`)
-    console.log(`App listening on port ${port}`)
+server.listen(PORT, async () => {
+    await mongoose.connect(MONGODB_URI, {useNewUrlParser: true})
+    console.log(`Database connected at ${MONGODB_URI}`)
+    console.log(`App listening on port ${PORT}`)
 });
--- seeds/index.js
+++ seeds/index.js
@@ -2,13 +2,15 @@ const mongoose = require('mongoose')
 const Movie = require('../models/Movie')
 const movies = require('./movies')
 require('dotenv').config()
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/hackerYouProject'
+
 
 const truncateDatabase = async () => {
   return Promise.all([Movie.deleteMany(), Movie.deleteMany()])
 }
 
 const makeSeeds = async () => {
-  await mongoose.connect(process.env.DB_URI, {useNewUrlParser: true})
+  await mongoose.connect(MONGODB_URI, {useNewUrlParser: true})
   await truncateDatabase()
   await Promise.all(movies.map(movie => movie.save()))
   mongoose.connection.close()
