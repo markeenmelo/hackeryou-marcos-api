9c5cc21dee6db5aa00f89b665cfba40540a4f0fe
Marcos Melo <marcos.melo@bell.ca>
2018-12-22 11:06:08 -0500

    removed dotenv

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 const Movie = require('../models/Movie')
 const movies = require('./movies')
-require('dotenv').config()
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/hackerYouProject'
 
 
