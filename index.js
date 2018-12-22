bb9ba44386a6ce2f4ddfa9c945922714f189a371
Marcos Melo <marcos.melo@bell.ca>
2018-12-22 01:00:31 -0500

    fixed env

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose')
 const server = require('./server')
 require('dotenv').config()
 
-server.listen(process.env.API_PORT, async () => {
+var port = process.env.PORT || 8080;
+
+
+server.listen(port, async () => {
     await mongoose.connect(process.env.DB_URI, {useNewUrlParser: true})
     console.log(`Database connected at ${process.env.DB_URI}`)
-    console.log(`App listening on port ${process.env.API_PORT}`)
+    console.log(`App listening on port ${port}`)
 });
